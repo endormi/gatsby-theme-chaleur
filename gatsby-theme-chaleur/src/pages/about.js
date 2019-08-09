f8e6d7d9c82971380610c66f109921b63b4e5c23
endormi <ernosalo14@gmail.com>
2019-08-09 12:17:33 +0300

    Add author, image and update styling

--- gatsby-theme-chaleur/src/pages/about.js
+++ gatsby-theme-chaleur/src/pages/about.js
@@ -1,10 +1,13 @@
 import React from "react"
 import "typeface-roboto"
 import "../components/css/style.css"
+import image from "../img/gatsby.png"
 
 export default () => {
     return (
-        <div><h1>About page!</h1>
+        <div>
+        <img src={image} style={{ width: `75px` }} alt="Logo" />
+        <h1>About page!</h1>
         <p>This is an simple about page!</p></div>
     )
 }
